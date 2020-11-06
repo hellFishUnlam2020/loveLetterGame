cab068ad840e048feb613836a48b6b1c7393dce5
NahuelHidalgo <nahuelhidalgo14@gmail.com>
2020-11-06 03:16:58 -0300

    cambios login/gameScreen

--- LoveLetter/src/view/GameScreen.java
+++ LoveLetter/src/view/GameScreen.java
@@ -1,24 +1,29 @@
 package view;
 
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import cards.Card;
-
-import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Font;
-import java.awt.EventQueue;
-
+import loveLetter.Deck;
 import viewCommunication.CardEligible;
 import viewCommunication.UserLoggable;
-import loveLetter.Deck;
 
 public class GameScreen implements UserLoggable, CardEligible {
 
 	private JFrame gameFrame;
+	private JPanel panel;
 	private boolean isLogged = false;
 	JLabel welcomeLabel;
 	JButton loginButton;
@@ -34,47 +39,137 @@ public class GameScreen implements UserLoggable, CardEligible {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		gameFrame = new JFrame();
-		gameFrame.setFont(new Font("Dialog", Font.PLAIN, 16));
-		gameFrame.setTitle("Love Letter");
-		gameFrame.getContentPane().setBackground(new Color(46, 139, 87));
-		gameFrame.getContentPane().setLayout(null);
-		gameFrame.setBounds(100, 100, 800, 581);
-		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		welcomeLabel = new JLabel("");
-		welcomeLabel.setBounds(61, 40, 203, 40);
-		welcomeLabel.setVisible(false);
-		gameFrame.getContentPane().add(welcomeLabel);
 		
-		loginButton = new JButton("Entrar");
-		loginButton.setBounds(367, 236, 117, 29);
-		loginButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				showLoginFrame();
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		gameFrame = new JFrame();
+		
+//		gameFrame.setFont(new Font("Dialog", Font.PLAIN, 16));
+//		gameFrame.setTitle("Love Letter");
+//		gameFrame.getContentPane().setBackground(new Color(46, 139, 87));
+//		gameFrame.setBounds(100, 100, 800, 581);
+//		gameFrame.setPreferredSize(JFrame.);
+//		gameFrame.getContentPane().setLayout(null);
+		
+		gameFrame.setResizable(false);
+		gameFrame.setUndecorated(true);
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		gameFrame.setSize(gd.getDisplayMode().getWidth(), gd.getDisplayMode().getHeight());
+		gameFrame.setLocationRelativeTo(null);
+	
+		panel = new JPanel();
+		panel.setBounds(0, 0, gameFrame.getWidth(), gameFrame.getHeight());
+		panel.setLayout(null);
+		
+		gameFrame.getContentPane().add(panel);
+		
+		
+		JButton playButton = new JButton("");
+		playButton.setContentAreaFilled(false);
+		playButton.setBorderPainted(false);
+		playButton.setIgnoreRepaint(true);
+		playButton.setOpaque(false);
+		playButton.setIcon(new ImageIcon(GameScreen.class.getResource("/images/Login/Capa 1.png")));
+		playButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		playButton.setBounds(839, 737, 242, 160);
+		playButton.setBorder(null);
+		panel.add(playButton);
+		
+		JButton profileButton = new JButton("");
+		profileButton.setContentAreaFilled(false);
+		profileButton.setBorderPainted(false);
+		profileButton.setIgnoreRepaint(true);
+		profileButton.setOpaque(false);
+		profileButton.setIcon(new ImageIcon(GameScreen.class.getResource("/images/Login/Capa 2.png")));
+		profileButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		profileButton.setBounds(889, 885, 120, 174);
+		profileButton.setBorder(null);
+		panel.add(profileButton);
+		
+		JButton configButton = new JButton("");
+		configButton.setContentAreaFilled(false);
+		configButton.setBorderPainted(false);
+		configButton.setIgnoreRepaint(true);
+		configButton.setOpaque(false);
+		configButton.setIcon(new ImageIcon(GameScreen.class.getResource("/images/Login/Capa 4.png")));
+		configButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		configButton.setBounds(1061, 965, 96, 94);
+		configButton.setBorder(null);
+		panel.add(configButton);
+
+		JButton exitButton = new JButton("");
+		exitButton.setContentAreaFilled(false);
+		exitButton.setBorderPainted(false);
+		exitButton.setIgnoreRepaint(true);
+		exitButton.setOpaque(false);
+		exitButton.setIcon(new ImageIcon(GameScreen.class.getResource("/images/Login/Capa 5.png")));
+		exitButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		exitButton.setBounds(1727, 83, 82, 84);
+		exitButton.setBorder(null);
+		exitButton.setToolTipText("Exit");
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameFrame.dispatchEvent(new WindowEvent(gameFrame, WindowEvent.WINDOW_CLOSING));
 			}
 		});
-		gameFrame.getContentPane().add(loginButton);
+		panel.add(exitButton);
 		
-		JButton btnNewButton = new JButton("Ver las cartas");
-		btnNewButton.setBounds(48, 348, 117, 29);
-		gameFrame.getContentPane().add(btnNewButton);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		JButton dadosButton = new JButton("");	
+		dadosButton.setContentAreaFilled(false);
+		dadosButton.setBorderPainted(false);
+		dadosButton.setIgnoreRepaint(true);
+		dadosButton.setOpaque(false);
+		dadosButton.setIcon(new ImageIcon(GameScreen.class.getResource("/images/Login/Capa 3.png")));
+		dadosButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		dadosButton.setBounds(763, 965, 104, 92);
+		dadosButton.setBorder(null);
+		dadosButton.setToolTipText("DadosButton");
+		dadosButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
 				showCardPickerFrame();
 			}
 		});
-
+		panel.add(dadosButton);
+		
+//		welcomeLabel = new JLabel("");
+//		welcomeLabel.setBounds(61, 40, 203, 40);
+//		welcomeLabel.setVisible(false);
+//		gameFrame.getContentPane().add(welcomeLabel);
+//
+//		loginButton = new JButton("Entrar");
+//		loginButton.setBounds(367, 236, 117, 29);
+//		loginButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//				showLoginFrame();
+//			}
+//		});
+//		gameFrame.getContentPane().add(loginButton);
+//
+//		JButton btnNewButton = new JButton("Ver las cartas");
+//		btnNewButton.setBounds(48, 348, 117, 29);
+//		gameFrame.getContentPane().add(btnNewButton);
+//		btnNewButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//				showCardPickerFrame();
+//			}
+//		});
+		
+		JLabel backgroundImage = new JLabel("");
+		backgroundImage.setIcon(new ImageIcon(GameScreen.class.getResource("/images/Fondo.png")));
+		backgroundImage.setBounds(0, 0, panel.getWidth(), panel.getHeight());
+		panel.add(backgroundImage);
+		
 	}
-	
 
 	public void showLoginFrame() {
 		LoginFrame loginFrame = new LoginFrame();
 		loginFrame.setUserLoggable(this);
+		loginFrame.setLocationRelativeTo(null);
 		loginFrame.setVisible(true);
-		loginFrame.setLocationRelativeTo(this.gameFrame);
 	}
-		
+
 	public void showCardPickerFrame() {
 		Deck deck = new Deck();
 		CardPickerFrame cardPicker = new CardPickerFrame(deck.getCards());
@@ -85,11 +180,11 @@ public class GameScreen implements UserLoggable, CardEligible {
 	}
 
 	public void startGame() {
-		if(!isLogged) {
+		if (!isLogged) {
 			showLoginFrame();
 		}
 	}
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -110,9 +205,9 @@ public class GameScreen implements UserLoggable, CardEligible {
 	@Override
 	public void userLogged(String name) {
 		isLogged = true;
-		welcomeLabel.setText("Bienvenido " + name);
-		welcomeLabel.setVisible(true);
-		loginButton.setVisible(false);
+//		welcomeLabel.setText("Bienvenido " + name);
+//		welcomeLabel.setVisible(true);
+//		loginButton.setVisible(false);
 	}
 
 	@Override
@@ -121,6 +216,6 @@ public class GameScreen implements UserLoggable, CardEligible {
 		cardPreview.setVisible(true);
 		cardPreview.setFocusable(true);
 		cardPreview.requestFocusInWindow();
-		cardPreview.init();		
+		cardPreview.init();
 	}
 }
--- LoveLetter/src/view/LoginFrame.java
+++ LoveLetter/src/view/LoginFrame.java
@@ -1,19 +1,23 @@
 package view;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.MatteBorder;
 
 import viewCommunication.UserLoggable;
-
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.Color;
 
 public class LoginFrame extends JFrame {
 
@@ -21,10 +25,11 @@ public class LoginFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = -2468719791156400022L;
-	private JPanel contentPane;
-	private JTextField nameTextField;
+	private JPanel logPanel;
+//	private JTextField nameTextField;
 	private UserLoggable userLoggable;
 	private JLabel loginErrorLabel;
+	JTextField userField;
 	
 	public void setUserLoggable(UserLoggable userLoggable) {
 		this.userLoggable = userLoggable;
@@ -34,55 +39,114 @@ public class LoginFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginFrame() {
+		ImageIcon image = new ImageIcon(LoginFrame.class.getResource("/Images/login.jpg"));
+		
 		setResizable(false);
-		setTitle("Antes de arrancar");
+		setUndecorated(true);
+//		setTitle("Antes de arrancar");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 416, 216);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		setPreferredSize(new Dimension(image.getIconWidth(), image.getIconHeight()));
+		setSize(image.getIconWidth(), image.getIconHeight());
 		
-		JLabel titleLabel = new JLabel("Login");
-		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		titleLabel.setBounds(177, 6, 61, 16);
-		contentPane.add(titleLabel);
+		Dimension frameSize = getSize();
+
+		logPanel = new JPanel();
+		logPanel.setBounds(0, 0, frameSize.width, frameSize.width);
+		logPanel.setLayout(null);
+		getContentPane().add(logPanel);
 		
-		JLabel nameLabel = new JLabel("Ingresa tu nombre:");
-		nameLabel.setBounds(50, 89, 130, 16);
-		contentPane.add(nameLabel);
+
+//		contentPane = new JPanel();
+//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+//		contentPane.setLayout(null);
+//		setContentPane(contentPane);
+		Dimension size = new Dimension(200, 50);
 		
-		nameTextField = new JTextField();
-		nameTextField.setBounds(222, 84, 173, 26);
-		contentPane.add(nameTextField);
-		nameTextField.setColumns(10);
+		userField = new JTextField();
+		userField.setForeground(Color.white);
+		userField.setText("UserName");
+		userField.setOpaque(false);
+		userField.setBorder(new MatteBorder(0, 0, 1, 0, new Color(255,255,255)));
+		userField.setBounds(frameSize.width/2-size.width/2, 5, size.width, size.height);
+		userField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (userField.getText().isEmpty()) {
+					userField.setText("UserName");
+				}
+			}
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (userField.getText().equals("UserName"))
+					userField.setText("");
+			}
+		});
+		logPanel.add(userField);
 		
-		JButton enterButton = new JButton("Entrar");
-		enterButton.setBounds(149, 146, 117, 29);
-		contentPane.add(enterButton);
+		JPasswordField passField = new JPasswordField();
+		passField.setForeground(Color.white);
+		passField.setText("contraseña");
+		passField.setOpaque(false);
+		passField.setBorder(new MatteBorder(0, 0, 1, 0, new Color(255,255,255)));
+		passField.setBounds(frameSize.width/2-size.width/2, size.height+5, size.width, size.height);
+		logPanel.add(passField);
 		
-		loginErrorLabel = new JLabel("New label");
-		loginErrorLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		loginErrorLabel.setVisible(false);
-		loginErrorLabel.setForeground(Color.RED);
-		loginErrorLabel.setBounds(6, 118, 404, 16);
-		contentPane.add(loginErrorLabel);
+//		JLabel titleLabel = new JLabel("Login");
+//		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//		titleLabel.setBounds(177, 6, 61, 16);
+//		contentPane.add(titleLabel);
 		
-		enterButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+//		JLabel nameLabel = new JLabel("Ingresa tu nombre:");
+//		nameLabel.setBounds(50, 89, 130, 16);
+//		contentPane.add(nameLabel);
+		
+//		nameTextField = new JTextField();
+//		nameTextField.setBounds(222, 84, 173, 26);
+//		contentPane.add(nameTextField);
+//		nameTextField.setColumns(10);
+		
+//		JButton enterButton = new JButton("Entrar");
+//		enterButton.setBounds(149, 146, 117, 29);
+//		contentPane.add(enterButton);
+		
+//		loginErrorLabel = new JLabel("New label");
+//		loginErrorLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//		loginErrorLabel.setVisible(false);
+//		loginErrorLabel.setForeground(Color.RED);
+//		loginErrorLabel.setBounds(6, 118, 404, 16);
+//		contentPane.add(loginErrorLabel);
+		
+		JButton enterButton = new JButton("Ingresar");
+		enterButton.setOpaque(false);
+		enterButton.setContentAreaFilled(false);
+		enterButton.setBorder(null);
+		enterButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		enterButton.setBounds(frameSize.width/2-size.width/2, size.height*2+25, size.width, size.height);
+		enterButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
 				if(isValidUserName()) {
-					userLoggable.userLogged(nameTextField.getText());
+					userLoggable.userLogged(userField.getText());
+//					JOptionPane.showConfirmDialog(null, "Bienvendo "+ userField.getText())
 					dispose();					
 				}
 			}
 		});
+		enterButton.requestFocusInWindow();
+		logPanel.add(enterButton);
+		JLabel backgroundLabel = new JLabel("");
+		backgroundLabel.setIcon(image);
+		backgroundLabel.setBounds(0, 0, image.getIconWidth(), image.getIconHeight());
+		logPanel.add(backgroundLabel);
+		
+		pack();
 	}
 	
 	private boolean isValidUserName() {
 		boolean validName = true;
 		
-		if(nameTextField.getText().isEmpty()) {
-			loginErrorLabel.setText("No podes tener un nombre vacÃ­o");
+		if(userField.getText().isEmpty()) {
+			loginErrorLabel.setText("No podes tener un nombre vacio");
 			loginErrorLabel.setVisible(true);
 			validName = false;
 		}
